2b73e2420508f482f34c1a59b6c7fcabdc32055b
Frederico Guilherme Klüser de Oliveira <kluserhuu@gmail.com>
2021-11-13 14:12:42 -0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    complexity: ["error", 5]
+  },
+};
