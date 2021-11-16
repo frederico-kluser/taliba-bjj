d0d3e2fcea133f72ca870234fbd5963e91c18c9b
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2021-11-16 07:30:17 -0300

    Finish

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+/* eslint-disable complexity */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-use-before-define */
+
+/*
+bell.play();
+*/
+
+const bell = new Audio('src/assets/sound/start-bell.mp3');
+const over = new Audio('src/assets/sound/acabou.mp3');
+
 let rounds = 0;
 let fightTime = 0;
 let restTime = 0;
 let firstClick = false;
 let started = false;
+
+let initialTime = 0;
+let actualRound = 0;
+let counter = null;
+
+let fightBell = true;
+let restBell = true;
+
+const timer = document.getElementsByClassName('background-timer')[0];
+const deskRound = document.getElementById('desk-round');
+const deskTime = document.getElementById('desk-time');
+const deskTimeText = document.getElementById('desk-time-h5');
+const deskTimeBackground = document.getElementById('desk-time-background');
 
 // eslint-disable-next-line complexity
 const main = () => {
@@ -31,16 +53,71 @@ const start = () => {
     }
 
     timerControl(fightTime, '100');
+    initialTime = new Date().getTime();
+    actualRound = rounds;
+    counter = setInterval(setIntervalFunc, 1);
   }, 500);
 };
 
+const setIntervalFunc = () => {
+  const currentTime = new Date().getTime();
+  const discount = Math.trunc((currentTime - initialTime) / 1000);
+  const fightCurrentTime = fightTime - discount;
+  const restCurrentTime = restTime + fightCurrentTime;
+  deskRound.innerHTML = actualRound < 10 ? `0${actualRound}` : actualRound;
+
+  if (fightCurrentTime > 0) {
+    if (fightBell) {
+      bell.play();
+      fightBell = false;
+      restBell = true;
+    }
+    deskTimeText.innerHTML = 'FIGHT TIME';
+    deskTimeBackground.removeAttribute('style');
+    const {minutes, seconds} = calcTime(fightCurrentTime);
+    deskTime.innerHTML = `${minutes}:${seconds}`;
+  } else if (restCurrentTime > 0) {
+    if (restBell) {
+      bell.play();
+      restBell = false;
+      fightBell = true;
+    }
+    deskTimeText.innerHTML = 'REST TIME';
+    deskTimeBackground.style.backgroundColor = '#00796B';
+    const {minutes, seconds} = calcTime(restCurrentTime);
+    deskTime.innerHTML = `${minutes}:${seconds}`;
+  } else if (actualRound > 1) {
+    restBell = true;
+    fightBell = true;
+    bell.play();
+    actualRound--;
+    initialTime = new Date().getTime();
+  } else {
+    over.play();
+    clean();
+  }
+};
+
 const timerControl = (time = '0', height = '0') => {
-  const timer = document.getElementsByClassName('background-timer')[0];
+  const {minutes, seconds} = calcTime(fightTime);
+  deskRound.innerHTML = rounds < 10 ? `0${rounds}` : rounds;
+  deskTime.innerHTML = `${minutes}:${seconds}`;
   timer.style.transitionDuration = `${time}s`;
   timer.style.height = `${height}%`;
 };
 
+const calcTime = time => {
+  let minutes = Math.trunc(time / 60);
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  let seconds = time % 60;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return {minutes, seconds};
+};
+
 const clean = () => {
+  clearInterval(counter);
   started = false;
   rounds = 0;
   fightTime = 0;
@@ -48,6 +125,8 @@ const clean = () => {
   document.getElementById('fightTime').innerHTML = '00:00';
   document.getElementById('restTime').innerHTML = '00:00';
   document.getElementById('rounds').innerHTML = '00';
+  deskTimeText.innerHTML = 'FIGHT TIME';
+  deskTimeBackground.removeAttribute('style');
   timerControl();
 
   // button-group opacity none
@@ -80,12 +159,7 @@ const fightTimeFunc = boolean => {
     fightTime -= 30;
   }
 
-  let minutes = Math.trunc(fightTime / 60);
-  minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-  let seconds = fightTime % 60;
-  seconds = seconds < 10 ? `0${seconds}` : seconds;
-
+  const {minutes, seconds} = calcTime(fightTime);
   document.getElementById('fightTime').innerHTML = `${minutes}:${seconds}`;
 };
 
@@ -96,12 +170,7 @@ const restTimeFunc = boolean => {
     restTime -= 30;
   }
 
-  let minutes = Math.trunc(restTime / 60);
-  minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-  let seconds = restTime % 60;
-  seconds = seconds < 10 ? `0${seconds}` : seconds;
-
+  const {minutes, seconds} = calcTime(restTime);
   document.getElementById('restTime').innerHTML = `${minutes}:${seconds}`;
 };
 
